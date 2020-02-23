13a5b9fff3075152a57465887dab0a38d6949a0e
John <38336418+jfsullivan1@users.noreply.github.com>
2020-02-23 14:17:35 -0500

    current ver
    updated

--- Code Handout/ImageLibrary/src/RasterImage.c
+++ Code Handout/ImageLibrary/src/RasterImage.c
@@ -14,55 +14,49 @@ void writeImage(char* path);
 RasterImage newImage(unsigned short width, unsigned short height, ImageType type,
 					 unsigned char wordSizeRowPadding)
 {
-    RasterImage * img = (RasterImage *)malloc((width * height) * sizeof(RasterImage) * wordSizeRowPadding);
-    img->height = height;
-    img->width = width;
-    img->type = type;
-    img->bytesPerRow = width * 4;
+    RasterImage img;
+    img.height = height;
+    img.width = width;
+    img.type = type;
 
     // with colored images we need 4 bytes per pixel. (RBGA)
     switch(type){
         case RGBA32_RASTER:
-            img->bytesPerRow = width * 4;
-            img->bytesPerPixel = BYTES_RBGA;
+            img.bytesPerPixel = BYTES_RBGA;
             break;
+
+        // I assign RBGA bytes per pixel here because a float is also 4 bytes. 
         case FLOAT_RASTER:
-            img->bytesPerRow = width * 4;
-            img->bytesPerPixel = BYTES_RBGA;
+            img.bytesPerPixel = BYTES_RBGA;
             break;
         case GRAY_RASTER:
-            img->bytesPerRow = width * 4;
-            img->bytesPerPixel = BYTES_GRAY;
+            img.bytesPerPixel = BYTES_GRAY;
             break;
         default:
             printf("Raster not supported");
             exit(1);
     }
+    img.bytesPerRow = (img.bytesPerPixel * width);
 
-    (unsigned char*)img->raster = (unsigned char*)malloc((width * height) * wordSizeRowPadding);
-    for(int i = 0; i < height; i++){
-        for(int j = 0; j < width; j++){
-            unsigned char* rgba = (unsigned char*)img->raster + img->bytesPerPixel*(i*height + j);
-            rgba = (unsigned char*)malloc(img->bytesPerPixel);
-        }
-    }
+    img.raster = (void*)calloc((width * height), img.bytesPerPixel);
     
-    return *img;
+
+    return img;
 }
 
 void readImage(char* path, unsigned short* width, unsigned short* height, ImageType imgType){
-    char extension = strrchr(path, '.');
+    char * extension = strrchr(path, '.');
     unsigned char* buffer;
     if(extension == ".tga"){
         FILE * tgaFile = fopen(path, "rb");
         if(tgaFile){
-            buffer = readTGA(path, &width, &height, &imgType);
-            RasterImage tgaImage = newImage(&width, &height, &imgType, WORDSIZE);
+            buffer = readTGA(path, width, height, &imgType);
+            RasterImage tgaImage = newImage(*width, *height, imgType, WORDSIZE);
         }
     }
 }
 
-void writeImage(FILE* path){
+void writeImage(char* path){
 
 }
 
